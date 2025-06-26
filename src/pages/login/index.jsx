6df4a3c6600225678f90b2e337752d0a4abe50ee
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-06-26 17:51:59 -0300

    update

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+
+export default function LoginScreen() {
+    return (
+        <>
+            <div className="bgImage">
+                <div className='row'>
+                    <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
+                        William
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
